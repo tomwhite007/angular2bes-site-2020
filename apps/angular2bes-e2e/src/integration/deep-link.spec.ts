15fae8ca65156c7b1df1b2b17a6a853584011259
Tom White <tomwhite007@gmail.com>
2020-08-19 07:40:49 +0100

    comment

--- apps/angular2bes-e2e/src/integration/deep-link.spec.ts
+++ apps/angular2bes-e2e/src/integration/deep-link.spec.ts
@@ -3,6 +3,12 @@ import { confirmSectionIsScrolledInView } from '../support/app.po';
 describe('deep linking to a page / section', () => {
   beforeEach(() => cy.visit('/testing-observables'));
 
+  /**
+   * This test relies on an external document being available which techinally should be mocked.
+   * However, I think this test is valid becuase it confirs the existence of a static external resource
+   * which is good to know via automated tests in a ci pipeline.
+   * See page-navigation.spec for proper external api mocking.
+   */
   it('should scroll to testing-observables section in less than 2 seconds', () => {
     cy.wait(2000); // for scroll to complete
 
