483e43b5914ca32ef9102c244b3c79734404e08d
Tom White <tomwhite007@gmail.com>
2021-01-21 08:34:53 +0000

    group name change

--- apps/angular2bes-e2e/src/integration/home.spec.ts
+++ apps/angular2bes-e2e/src/integration/home.spec.ts
@@ -2,8 +2,6 @@ describe('home page', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display home page title', () => {
-    cy.get('app-home h1')
-      .contains('Brighton / Worthing')
-      .contains('Angular 2+ Meetup');
+    cy.get('app-home h1').contains('Angular Sussex');
   });
 });
